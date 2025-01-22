363e836fc01ffe6b25f7838a7fdc688211d2ce72
Steffen Hirte <shirte@users.noreply.github.com>
2025-01-22 19:17:07 +1300

    Fix RDKit version in dependencies

--- setup.py
+++ setup.py
@@ -1,19 +1,5 @@
 from setuptools import find_packages, setup
 
-# some RDKit versions are not recognized by setuptools
-# -> check if RDKit is installed by attempting to import it
-# -> if RDKit can be imported, do not add it to install_requires
-rdkit_installed = False
-try:
-    import rdkit
-
-    rdkit_installed = True
-except ImportError:
-    pass
-
-# rdkit 2022.3.3 is the oldest (reasonable) version
-rdkit_requirement = ["rdkit>=2022.3.3"] if not rdkit_installed else []
-
 setup(
     name="hitdexter",
     version="0.3.1",
@@ -23,13 +9,13 @@ setup(
     url="https://github.com/molinfo-vienna/hitdexter",
     long_description=open("README.md").read(),
     include_package_data=True,
-    install_requires=rdkit_requirement
-    + [
+    install_requires=[
+        "rdkit==2020.09.1",
         "scikit_learn==0.23.2",
         "numpy==1.19.2",
         "nerdd-module>=0.3.6",
-        "fpsim2==0.4.0",
-        "chembl_structure_pipeline==1.1.0",
+        "fpsim2==0.2.8",
+        "chembl_structure_pipeline==1.0.0",
         # install importlib-resources and importlib-metadata for old Python versions
         "importlib-resources>=5; python_version<'3.9'",
         "importlib-metadata>=4.6; python_version<'3.10'",
